92bc304e838b7bc772cb83d5f8ad6c0a2bf01645
Pritam Singh <shekhawatkuldeep2806@gmail.com>
2026-04-10 23:02:02 +0530

    Added the Firebase Functionality

--- lib/grocery_list.dart
+++ lib/grocery_list.dart
@@ -1,45 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/grocery_item.dart';
 import 'package:shopping_list_app/new_item.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list_app/categories.dart';
 
-class GroceryList extends StatefulWidget {                      // Stateful widget because 
+
+class GroceryList extends StatefulWidget {                     // Stateful Widget
   const GroceryList({super.key});
 
   @override
-  State<GroceryList> createState() {
-    return _GroceryListState();                  // Creates _GroceryListState to manage the widget's state
-  }    
+  State<GroceryList> createState() => _GroceryListState();     // Creates _GroceryListState to manage the widget's state
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];                        // List of our added items
+  var _isLoading = true;                                       // Whether the app is loading or not
+  String? _error;                                              // Error or not [ initially null ]
 
-  // Hum jab newItem screen par jaayege, then uss time humaare paas value nahi hogi (Hence async use kar rahe (Promise))
-  void _addItem() async {                        // async because navigation returns a future
-    final newItem = await Navigator.of(context).push<GroceryItem>(               
-      MaterialPageRoute(                         // Redirecting to new page [ Navigation ]
-        builder: (ctx) => const NewItem(),       // Navigator.push(): Opens "NewItem" screen as a new route
+  @override
+  void initState() {                // Overriding the initState method (which runs once when the widget is first created)
+    super.initState();              // Fetching the data from the Firebase
+    _loadItems();                   // Hence when we open the app for the first time then Items should appear.
+  }
+
+  // "async" used because of the future promise (We don't want to block the UI)(Because the network call takes time).
+  void _loadItems() async {                                    // Fetches the data from the "Firebase Realtime Database".
+    final url = Uri.https('flutter-apps-26500-default-rtdb.firebaseio.com', 'shopping-list.json');
+    // (Builds Firebase Realtime Database URL). Firebase URL to fetch the shopping list data.
+
+    try {                                                      // "try" used to handle the potential errors (Like the network erros)
+      final response = await http.get(url);                    // HTTP Get request to the URL and waits for the response
+      // Above "response" handles all the things that we have obtained from the Firebase.
+
+      if (response.statusCode >= 400) {                        // Preparing for the error in getting the response
+        setState(() {                                          // UI Update
+          _error = 'Failed to fetch data. Please try again later.';
+          _isLoading = false;                                  // No more further uploads
+        });
+        return;                                                // Stops the execution
+      }
+
+      if (response.body == 'null') {                           // When there is nothing to display, then Firebase return the string 'null
+        setState(() {
+          _isLoading = false;
+        });
+        return;                                                // Stops the execution
+      }
+
+      final Map<String, dynamic> listData = json.decode(response.body);      // Decodes the JSON response into a map
+      // dynamic means "anytype". [ Above converts Json to dart map. ]
+      final List<GroceryItem> loadedItems = [];                // Temporary list to store the parsed data.
+
+      for (final item in listData.entries) {                   // Looping through the map.
+        final category = categories.entries                    // Finding the matching category object
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])   // Compare category title with stored string
+            .value;                                            // returns the matching category
+        loadedItems.add(                                       // Adding the groceryitem in our list
+          GroceryItem(
+            id: item.key,                                      // Uses the firebase "key" as ID
+            name: item.value['name'],                          // Map ke andar Dictionary or map types
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
+      setState(() {
+        _groceryItems = loadedItems;                           // _groceryItems are displayed on the screen
+        _isLoading = false;
+      });
+    } 
+    catch (error) {                                            // Catching the error
+      setState(() {
+        _error = 'Something went wrong! Please try again later.';
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(       // Waits for the "newItem"
+      MaterialPageRoute(                                       // Navigates to the NewItem and awaits result (GroceryItem or null).
+        builder: (ctx) => const NewItem(),                     // Opens or navigates to the AddItem screen.
       ),
     );
 
-    if (newItem == null) {                       // Return nothing or do nothing on the main screen 
+    if (newItem == null) {
       return;
     }
 
-    setState(() {                                // setState(): Triggers UI rebuild to show the new item
-      _groceryItems.add(newItem);
+    setState(() {
+      _groceryItems.add(newItem);                              // Added the new Item in the grocery list.
     });
   }
 
-  void _removeItem(GroceryItem item) {
-    setState(() {                                // setState(): Triggers UI rebuild to show the new item
-      _groceryItems.remove(item);
+  void _removeItem(GroceryItem item) async {                   // Async method to remove an item.
+    final index = _groceryItems.indexOf(item);                 // Stores the item index for roll_back
+    setState(() {
+      _groceryItems.remove(item);                              // Remove the item from the list
     });
+
+    // Constructs the URL for deleting the specific item from Firebase.
+    final url = Uri.https('flutter-apps-26500-default-rtdb.firebaseio.com','shopping-list/${item.id}.json');
+
+    final response = await http.delete(url);                   // Send a delete request
+
+    if (response.statusCode >= 400) {
+      // Optional: Show error message
+      setState(() {
+        _groceryItems.insert(index, item);                     // If error occurs, then again insert it.
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(child: Text('No items added yet.'));          // Text shown when no Shopping item is there
+    Widget content = const Center(child: Text('No items added yet.'));      // Text shown when no Shopping item is there
+
+    if (_isLoading) {                                                           // We don't get error in getting response from Firebase.
+      content = const Center(child: CircularProgressIndicator());               // ProgressIndicator
+    }
 
     if (_groceryItems.isNotEmpty) {                                             // When we have List items 
       content = ListView.builder(                                               // Efficiently builds list items on demand
@@ -64,12 +145,16 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
 
+    if (_error != null) {
+      content = Center(child: Text(_error!));                                   // Showing the error, When we have one
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [
           IconButton(
-            onPressed: _addItem,                       // When we press the AppBar Plus icon, then we move to newItem screen
+            onPressed: _addItem,               // When we press the AppBar Plus icon, then we move to "newItem" screen
             icon: const Icon(Icons.add),
           ),
         ],
--- lib/new_item.dart
+++ lib/new_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:shopping_list_app/categories.dart';
 import 'package:shopping_list_app/category.dart';
 import 'package:shopping_list_app/grocery_item.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;                   // "flutter pub add http" in the terminal.
 
-class NewItem extends StatefulWidget {                  // Stateful as UI Updates, as the user types
+class NewItem extends StatefulWidget {                     // Stateful as UI Updates, as the user types
   const NewItem({super.key});
 
   @override
@@ -15,20 +16,50 @@ class NewItem extends StatefulWidget {                  // Stateful as UI Update
 
 class _NewItemState extends State<NewItem> {
   // GlobalKey<FormState>() creates a unique key that can access the state of a "Form" widget anywhere in the widget tree
-  final _formKey = GlobalKey<FormState>();                               // Tracks form validation and saving (Global_Key)
+  final _formKey = GlobalKey<FormState>();                             // Tracks "form" validation and saving (Global_Key)
   var _enteredName = '';
   var _enteredQuantity = 1;
-  var _selectedCategory = categories[Categories.vegetables]!;            // Default Category
+  var _selectedCategory = categories[Categories.vegetables]!;          // Default Category
+  var _isSending = false;
 
-  // "Validating" the form means checking each input field to make sure the entered data is acceptable before saving it.
-  void _saveItem() {
-    if (_formKey.currentState!.validate()) {                             // Validate the form (When we have null form validators)
-      _formKey.currentState!.save();                                     // Save field values into state vars
-      Navigator.of(context).pop(                                         // Going back where they came from
-        GroceryItem(                                                     // Taking back the written values
-          id: DateTime.now().toString(),                                 // Way to provide the unique id
-          name: _enteredName,
-          quantity: _enteredQuantity,
+  void _saveItem() async {                                             // "Async" Function
+    // "Validating" the form means checking each input field to make sure the entered data is acceptable before saving it
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();                                   // Save the values if values are validated
+      setState(() {
+        _isSending = true;
+      });
+
+      // Building a secure HTTPS URL for the Firebase Realtime Database endpoint. Resource path "shopping-list.json" is also used
+      final url = Uri.https('flutter-apps-26500-default-rtdb.firebaseio.com', 'shopping-list.json');
+      final response = await http.post(                       // Sending a POST request to Firebase
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(                                    // Dart Map into json string
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
+
+      // Decodes the HTTP response body from JSON into a Dart map.
+      // resData now contains Firebase’s response, usually with the generated item ID.
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      // Checks whether the current state object is "still mounted" in the widget tree.
+      // Essential after an await, because the widget may have been removed while the network request was in flight.
+      // If the widget is no longer mounted, it returns early and avoids using context.
+      if (!mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop(                   // Go back to the Main screen with the added data
+        GroceryItem(                               // See the "NewItem Async function". Ussi ka data hai
+          id: resData['name'],                     // Way to provide the unique id
+          name: _enteredName,                      // Taking back the written values
+          quantity: _enteredQuantity,              
           category: _selectedCategory,
         ),
       );
@@ -44,10 +75,10 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
-          key: _formKey,                                        // Important line (What will be used as key)
+          key: _formKey,                                              // Important line (What will be used as key)
           child: Column(
             children: [
-              TextFormField(                                    // Type of container to get user input
+              TextFormField(                                          // Type of container to get user input
                 maxLength: 50,
                 decoration: const InputDecoration(
                   label: Text('Name'),
@@ -62,18 +93,18 @@ class _NewItemState extends State<NewItem> {
                   return null;
                 },
                 onSaved: (value) {
-                  _enteredName = value!;                         // onSaved stores the name
+                  _enteredName = value!;                              // onSaved stores the name
                 },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
-                    child: TextFormField(                        // User input
+                    child: TextFormField(                             // User input
                       decoration: const InputDecoration(
                         label: Text('Quantity'),
                       ),
-                      keyboardType: TextInputType.number,        // Changing the keyboard type to just number
+                      keyboardType: TextInputType.number,             // Number Keyboard type
                       initialValue: _enteredQuantity.toString(),
                       validator: (value) {
                         if (value == null ||
@@ -85,13 +116,13 @@ class _NewItemState extends State<NewItem> {
                         return null;
                       },
                       onSaved: (value) {
-                        _enteredQuantity = int.parse(value!);         // int to string
+                        _enteredQuantity = int.parse(value!);          // int to string
                       },
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
-                    child: DropdownButtonFormField(                   // Another type of Dropdown (Modern)
+                    child: DropdownButtonFormField(                    // Another type of Dropdown (Modern)
                       value: _selectedCategory,
                       items: [
                         for (final category in categories.entries)
@@ -124,14 +155,22 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();           // Reset all the things
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();              // Reset all the things
+                          },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,                       // Save the things
-                    child: const Text('Add Item'),
+                    onPressed: _isSending ? null : _saveItem,            // Save a thing
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : const Text('Add Item'),
                   )
                 ],
               ),
@@ -142,3 +181,21 @@ class _NewItemState extends State<NewItem> {
     );
   }
 }
+
+/*
+
+class _NewItemState extends State<NewItem> {
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {                             // Validate the form (When we have null form validators)
+      _formKey.currentState!.save();                                     // Save field values into state vars
+      Navigator.of(context).pop(                                         // Going back where they came from
+        GroceryItem(                                                     // Taking back the written values
+          id: DateTime.now().toString(),                                 
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
+    }
+  }
+*/
